68e6c9ca27ffca0e5aefe68bd9c0e745467803b8
ischeinkman <scheinkman.ilan@gmail.com>
2015-11-28 17:36:40 -0800

    -Sped up Colorbook loading through shifting PDF opening to an async task and getting the size from a constant instead of calculating -Added GenderedConstants class to store all constants that could change between AZA and BBG versions of the app (like colorbook size)

---
+++ app/src/main/java/org/ramonaza/unofficialazaapp/helpers/backend/GenderedConstants.java
@@ -0,0 +1,11 @@
+package org.ramonaza.unofficialazaapp.helpers.backend;
+
+/**
+ * Created by ilan on 11/28/15.
+ */
+public final class GenderedConstants {
+    public static final int COLORBOOK_SIZE = 84;
+
+    private GenderedConstants() {
+    }
+}
