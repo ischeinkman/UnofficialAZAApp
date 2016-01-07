6e99d6b336851b30b58b437b681e8e5086457e54
ischeinkman <scheinkman.ilan@gmail.com>
2016-01-06 21:33:03 -0800

    -EventListFragment now reads from the internal Events database -EventNotificationService now provides a public interface for updating events

--- app/src/main/java/org/ramonaza/unofficialazaapp/events/ui/fragments/EventListFragment.java
+++ app/src/main/java/org/ramonaza/unofficialazaapp/events/ui/fragments/EventListFragment.java
@@ -1,14 +1,28 @@
 package org.ramonaza.unofficialazaapp.events.ui.fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import org.ramonaza.unofficialazaapp.events.ui.activities.EventPageActivity;
-import org.ramonaza.unofficialazaapp.helpers.backend.ChapterPackHandlerSupport;
+import org.ramonaza.unofficialazaapp.helpers.backend.EventNotificationService;
 import org.ramonaza.unofficialazaapp.helpers.ui.fragments.InfoWrapperListFragStyles.InfoWrapperTextListFragment;
+import org.ramonaza.unofficialazaapp.people.backend.EventDatabaseHandler;
 import org.ramonazaapi.events.EventInfoWrapper;
-import org.ramonazaapi.events.EventRSSHandler;
 import org.ramonazaapi.interfaces.InfoWrapper;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /*
  * Created by Ilan Scheinkman
@@ -17,8 +31,21 @@ public class EventListFragment extends InfoWrapperTextListFragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private EventRSSHandler handler;
+    private EventDatabaseHandler handler;
+    private EventNotificationService updateService;
+    private boolean serviceBound;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            updateService = ((EventNotificationService.MyBinder) iBinder).getService();
+            serviceBound = true;
+        }
 
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            serviceBound = false;
+        }
+    };
 
     public static EventListFragment newInstance(int sectionNumber) {
         EventListFragment fragment = new EventListFragment();
@@ -29,37 +56,55 @@ public class EventListFragment extends InfoWrapperTextListFragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        Intent intent = new Intent(getActivity(), EventNotificationService.class);
+        getActivity().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (serviceBound) {
+            getActivity().unbindService(mConnection);
+        }
+        serviceBound = false;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View superView = super.onCreateView(inflater, container, savedInstanceState);
+        return superView;
+    }
+
+    @Override
     public void onButtonClick(InfoWrapper mWrapper) {
         if (mWrapper.getId() < 0) return;
         Intent intent = new Intent(getActivity(), EventPageActivity.class);
-        intent.putExtra(EventPageActivity.EVENT_DATA, handler.getEventRSS(mWrapper.getId()));
+        intent.putExtra(EventPageActivity.EVENT_DATA, mWrapper.getId());
         startActivity(intent);
     }
 
     @Override
     public InfoWrapper[] generateInfo() {
-        if (!ChapterPackHandlerSupport.chapterPackIsLoaded() && ChapterPackHandlerSupport.getOptions().length > 0) {
-            ChapterPackHandlerSupport.getChapterPackHandler(getActivity(), ChapterPackHandlerSupport.getOptions()[0]);
+        while (!serviceBound) ;
+        updateService.updateEventsSync();
+        handler = new EventDatabaseHandler(getActivity());
+        //TODO: Store event dates as MYSQL Date objects to allow for WHERE clause filtering
+        EventInfoWrapper[] allEvents = handler.getEvents(null, null);
+        DateFormat df = new SimpleDateFormat("E, M d");
+        Date current = Calendar.getInstance().getTime();
+        ArrayList futureEvents = new ArrayList();
+        for (EventInfoWrapper wrapper : allEvents) {
+            try {
+                Date eventDate = df.parse(wrapper.getDate());
+                if (eventDate.after(current)) futureEvents.add(wrapper);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                futureEvents.add(wrapper); //Just in case someone's date format is off
+            }
         }
-        handler = ChapterPackHandlerSupport.getEventHandler(getActivity());
-        if (handler == null) {
-            EventInfoWrapper noLoadedHandler = new EventInfoWrapper();
-            noLoadedHandler.setName("Please download a Chapter Pack to access this feature.");
-            noLoadedHandler.setId(-1);
-            return new EventInfoWrapper[]{noLoadedHandler};
-        }
-        EventInfoWrapper[] handlerEvents = handler.getEventsFromRss();
-        if (handlerEvents == null) {
-            EventInfoWrapper nullEvent = new EventInfoWrapper();
-            nullEvent.setName("An error occured. Please try again later.");
-            nullEvent.setId(-1);
-            return new EventInfoWrapper[]{nullEvent};
-        } else if (handlerEvents.length == 0) {
-            EventInfoWrapper noEvent = new EventInfoWrapper();
-            noEvent.setName("No events posted.");
-            noEvent.setId(-1);
-            return new EventInfoWrapper[]{noEvent};
-        } else return handlerEvents;
+        return (EventInfoWrapper[]) futureEvents.toArray(new EventInfoWrapper[futureEvents.size()]);
     }
 
 
